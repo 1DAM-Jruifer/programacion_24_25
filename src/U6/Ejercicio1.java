ca59b3378929e357e1a39d9593b23bc81efc5724
unknown <1DAM-javierrui245@iesmartinezm.es>
2025-03-19 18:54:38 +0100

    meteo

--- src/U6/Ejercicio1.java
+++ src/U6/Ejercicio1.java
@@ -1,12 +1,4 @@
-//Actividad 1: Realizar un programa que solicite al usuario el nombre de un fichero de texto
-// y muestre su contenido en pantalla. Si no se proporciona ningún nombre de fichero,
-// la aplicación utilizará por defecto prueba.txt.
-
 package U6;
 
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Ejercicio1 {
-
-    }
+}
